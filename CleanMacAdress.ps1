622f709b71f6703e252b1b39bfeba013190a8ae5
rtpows <126966153+rtpows@users.noreply.github.com>
2023-03-14 11:15:34 -0400

    Add files via upload

---
+++ CleanMacAdress.ps1
@@ -0,0 +1,62 @@
+﻿function Clean-MacAddress {
+
+[OutputType([String], ParameterSetName = "Upper")]
+    [OutputType([String], ParameterSetName = "Lower")]
+    [CmdletBinding(DefaultParameterSetName = 'Upper')]
+    param
+    (
+        [Parameter(ParameterSetName = 'Lower')]
+        [Parameter(ParameterSetName = 'Upper')]
+        [String]$MacAddress,
+
+        [Parameter(ParameterSetName = 'Lower')]
+        [Parameter(ParameterSetName = 'Upper')]
+        [ValidateSet(':', 'None', '.', "-")]
+        $Separator,
+
+        [Parameter(ParameterSetName = 'Upper')]
+        [Switch]$Uppercase,
+
+        [Parameter(ParameterSetName = 'Lower')]
+        [Switch]$Lowercase
+    )
+
+    BEGIN {
+        # Initial Cleanup
+        $MacAddress = $MacAddress -replace "-", "" #Replace Dash
+        $MacAddress = $MacAddress -replace ":", "" #Replace Colon
+        $MacAddress = $MacAddress -replace "/s", "" #Remove whitespace
+        $MacAddress = $MacAddress -replace " ", "" #Remove whitespace
+        $MacAddress = $MacAddress -replace "\.", "" #Remove dots
+        $MacAddress = $MacAddress.trim() #Remove space at the beginning
+        $MacAddress = $MacAddress.trimend() #Remove space at the end
+    }
+    PROCESS {
+        IF ($PSBoundParameters['Uppercase']) {
+            $MacAddress = $macaddress.toupper()
+        }
+        IF ($PSBoundParameters['Lowercase']) {
+            $MacAddress = $macaddress.tolower()
+        }
+        IF ($PSBoundParameters['Separator']) {
+            IF ($Separator -ne "None") {
+                $MacAddress = $MacAddress -replace '(..(?!$))', "`$1$Separator"
+            }
+        }
+    }
+    END {
+        
+     
+        Write-Output $MacAddress
+
+        $RegEx = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9A-Fa-f]{2}){6}$"
+	if ($MacAddress -match $RegEx) {
+		return $true
+        Write-Output $MacAddress
+	} else {
+		return $false
+        Write-Output "$MacAddress"
+	}
+    }
+}
+
